98c0350541a0de81c477afe221401c5c303a1ca8
lizhou828 <lizhou828@126.com>
2018-04-15 19:20:29 +0800

    first commit

---
+++ test1.c
@@ -0,0 +1,10 @@
+//#include <stdio.h>
+//int sum(){
+//    int a ,b ,sum; /*  声明3个变量*/
+//    a = 1 ;
+//    b = 2;
+//    sum = a + b;
+//    printf(" a add b is : %d \n",sum);
+//    return 0;
+//}
+//
